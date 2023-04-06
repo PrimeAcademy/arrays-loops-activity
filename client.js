086c8072696637ec0568852d822c7487e3acd909
Hannah Engelhart <hannah.engelhart@gmail.com>
2023-04-05 20:57:32 -0500

    finished half the questions

--- client.js
+++ client.js
@@ -2,11 +2,16 @@
  * #1 Hobbies
  * ----------
  * 1. Create an array of group member hobbies
+ 
  * 2. Write a loop that logs out each hobby
  * 3. After the loop, log out the total number of
  *    hobbies
  */
-
+let groupMemberHobbies = ['crochet', 'gym', 'reading', 'disc golf'];
+for (let hobbies of groupMemberHobbies) {
+    console.log(hobbies);
+}
+console.log('Total hobbies:', groupMemberHobbies.length);
 
 // Example output
 // 1. swimming
@@ -24,7 +29,15 @@
  *    is in the array
  * 4. Output the array and number of times teal was found
  */
-
+// let sum = 0;
+let colors = ['red', 'orange', 'yellow', 'green','teal', 'teal', 'teal'];
+let tealCount = 0;
+for (let item of colors){
+    if(item === 'teal'){
+        tealCount += 1;
+    }
+}
+console.log(tealCount)
 
 // Example output
 // green, red, teal, orange, teal
@@ -41,6 +54,22 @@
  * 4. Output the original array, odd number array and even number array
  */
 
+const numbers = [1, 2, 3, 4, 5, 6];
+let oddNumbers = [];
+let evenNumbers = [];
+for (let num of numbers) {
+    if (num % 2 === 0) {
+       evenNumbers.push(num)
+        } else {(num % 2 === 1) 
+           oddNumbers.push(num) 
+        }
+       
+    } 
+    console.log(numbers);
+    console.log('Odd Numbers', oddNumbers);
+    console.log('Even Numbers', evenNumbers);
+
+
 
 // Example output
 // 3, 7, 2, 8, 11, 4, 2
@@ -56,6 +85,8 @@
  * 3. Write a loop that adds the opposite value to the toggled array
  * 4. Output both arrays
  */
+let booleanArray = [true, false, true, false, false];
+let toggled = [];
 
 
 // Example output
